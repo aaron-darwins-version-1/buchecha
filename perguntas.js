9726fbf5e0bbaa8792b95227bd58710370ed0269
aaron nunez'version <140652987+aaron-darwins-version-1@users.noreply.github.com>
2024-11-13 11:41:06 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos ermos de uma P.A',
+    'Utiliz-se a fórmula: ((a1+an).q)/2)'
+)
+criaCartao(
+    'Química',
+    'Qual a formula da química da água',
+    'H2O'
+)
+criaCartao( 
+    'Português',
+    'Quem foi Dom Casmurro?',
+    'Personagem de Machado de Assis'
+)
+criaCartao(    
+    'Hiatória',
+    'Em que ano ocorreu a Proclamação da República',
+    '1889'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá',
+    'Ottawa'
+)
+criaCartao(
+    'História',
+    'Quem é conhecido como o pai da matemática',
+    'Pitágoras'
+)
+criaCartao(
+    'História',
+    'Em que ano começou a Segumda Guerra Mundial?',
+    '1939'
+)
+criaCartao(
+    'Matemática',
+    'Qual a formula da área de um triângulo?',
+    'A=(base x altura)/2'
+)
+criaCartao(
+    'Física',
+    'Qual a seguda lei de Newton?',
+    'F = m x a(força = massa x aceleração)'
+)
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos EUA',
+    'George Washingthon'
+)
+criaCartao(
+    'Ciência',
+    'Qual é o maior planta do Sistema Solar',
+    'Júpiter'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'Paris'
+)
+criaCartao(
+    'Ciência',
+    'O que é fotossítese',
+    'Processo pelo qual as planestas produzem alimento por meio da luz solar, água e dióxido de carbono'
+)
+criaCartao(
+    'História',
+    'Em que ano foi assinada a Lei Áurea no Brasil',
+    '1888'
+)
+criaCartao(
+    'Português',
+    'Quem escreveu "Os Lusíadas"',
+    'Luís de Camões'
+)
+criaCartao(
+    'Química',
+    'Qual é a formula química do dióxido de carbono',
+    'CO2'
+)
+criaCartao(
+        'História',
+        'Quem foi o pricipal líder da Revolução Russa de 1917',
+        'Vladimir Lenin'
+)
+criaCartao(
+    'Química',
+    'Qual é o número atômico do oxigênio',
+    '8'
+)
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Revolução Francesa',
+    '1917'
+)
+criaCartao(
+    'Química',
+    'Quais são as bases nitrogenadas do DNA',
+    "Ademina(A), Timina(T), Citosina(C) e Guanina(G)"
